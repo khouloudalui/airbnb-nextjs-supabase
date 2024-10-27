597bdaef183547425d8568aa9b3518b09268bcfb
kiki <aloui.khouloud11@gmail.com>
2024-10-27 17:20:47 +0100

    feat:add Kinde authentication integration

---
+++ app/components/UserNav.tsx
@@ -0,0 +1,55 @@
+/* eslint-disable @next/next/no-img-element */
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { MenuIcon } from "lucide-react";
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+
+async function UserNav() {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+console.log("user",user)
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="rounded-full border px-2 lg:px-4 lg:py-2 flex items-center gap-x-3">
+          <MenuIcon className="w-6 h-6 lg:w-5 lg:h-5" />
+          <img
+            src={user?.picture ?? "https://static.vecteezy.com/system/resources/thumbnails/009/292/244/small/default-avatar-icon-of-social-media-user-vector.jpg"}
+            alt=""
+            className="rounded-full h-8 w-8 hidden lg:block"
+          />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end" className="w-[200px]">
+        {user ? (
+          <>
+            <DropdownMenuItem>
+              <LogoutLink className="w-full">Logout</LogoutLink>
+            </DropdownMenuItem>
+          </>
+        ) : (
+          <>
+            <DropdownMenuItem>
+              <LoginLink className="w-full">Sign in</LoginLink>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <RegisterLink className="w-full">Sign up</RegisterLink>
+            </DropdownMenuItem>
+          </>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default UserNav;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+
 export default function Home() {
-  return <h1>hello</h1>;
+  return <div>hello</div>;
 }
